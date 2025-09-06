76d4875c554dc73c131c7f5bc3fd1d74a767800f
ogsahil <contactsahilpnayak@gmail.com>
2025-09-06 14:27:56 +0530

    admin panel : registered

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from .models import Student
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(SimpleHistoryAdmin):
+    list_display = (
+        'roll_number', 'first_name', 'last_name', 'academic_year', 'degree_type', 'admission_date'
+    )
+    search_fields = ('first_name', 'last_name', 'roll_number')
+    list_filter = ('academic_year', 'degree_type', 'state', 'city')
